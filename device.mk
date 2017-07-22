9ec79a00265974c303fd1ebcba4f25bcb1e51cdd
rohitporoli <rohitp4all@gmail.com>
2017-07-22 22:00:00 +0530

    initialDoze

--- device.mk
+++ device.mk
@@ -380,3 +380,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/pixelstyle/etc/sysconfig/pixel_2017.xml:system/etc/sysconfig/pixel_2017.xml \
     $(LOCAL_PATH)/pixelstyle/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
     $(LOCAL_PATH)/pixelstyle/etc/sysconfig/google_build.xml:system/etc/sysconfig/google_build.xml
+
+# Doze
+PRODUCT_PACKAGES += \
+    XiaomiDoze
