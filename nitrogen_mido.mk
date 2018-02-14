cd2418a67e1c08f42b7d0b4736abc76eef461188
Ravi Pingat <pingatravi@gmail.com>
2018-02-14 18:14:49 +0000

    Nitrogen

--- nitrogen_mido.mk
+++ nitrogen_mido.mk
@@ -20,9 +20,9 @@ $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 TARGET_ARCH := arm64
 TARGET_DENSITY := xxhdpi
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/nitrogen/product*/common.mk)
 
-PRODUCT_NAME := aosp_mido
+PRODUCT_NAME := nitrogen_mido
 BOARD_VENDOR := Xiaomi
 PRODUCT_DEVICE := mido
 
